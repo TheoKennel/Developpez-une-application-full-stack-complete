695c406e69b1e0ad9ce582435d1c17014edaf420
TheoKennel <theokennel@gmail.com>
2024-05-08 16:05:03 +0200

    fixed bug cant get coverage cypress

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   pageLoadTimeout: 5000,
   defaultCommandTimeout : 5100,
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts').default(on, config)
+    },
     baseUrl: 'http://localhost:4200',
   },
   env: {
